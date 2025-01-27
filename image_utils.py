cbb62b9026ad94dd0bd74f19ade5f14f21ac6f6b
shira1-code <shirabodek12@gmail.com>
2025-01-27 12:29:57 +0200

    Update image_utils.py

--- image_utils.py
+++ image_utils.py
@@ -3,7 +3,13 @@ import numpy as np
 from scipy.signal import convolve2d
 
 def load_image(path):
-    pass # Replace the `pass` with your code
+from PIL import Image
+import numpy as np
+import matplotlib.pyplot as plt
+
+load_image = Image.open('Thailand.jpg')
+image = np.array(load_image)
+plt.imshow(load_image);
 
 def edge_detection(image):
     pass # Replace the `pass` with your code
